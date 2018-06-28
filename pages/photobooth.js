d75be5a30fa0e64b6bd683c55a277e373022a5f3
Kaho Cheung <kaho@elefanttraks.com>
2018-06-28 14:31:12 +1000

    prettifeid

--- pages/photobooth.js
+++ pages/photobooth.js
@@ -6,33 +6,30 @@ import Webcam from 'react-webcam';
 import './photobooth.css';
 
 class Home extends Component {
-  setRef = (webcam) => {
-    this.webcam = webcam;
-  }
+	setRef = (webcam) => {
+		this.webcam = webcam;
+	};
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			screenshot: null,
+			tab: 0,
+		};
+	}
+	handleClick = () => {
+		const screenshot = this.webcam.getScreenshot();
+		this.setState({ screenshot });
+	};
 
-constructor(props) {
-        super(props);
-        this.state = {
-          screenshot: null,
-          tab: 0
-        };
-      }
-      handleClick = () => {
-        const screenshot = this.webcam.getScreenshot();
-        this.setState({ screenshot });
-      }
-
-
-
-  capture = () => {
-        const screenshot = this.webcam.getScreenshot();
-        this.setState({ screenshot });
-  };
+	capture = () => {
+		const screenshot = this.webcam.getScreenshot();
+		this.setState({ screenshot });
+	};
 
 	componentDidMount() {
-//		this.webcam = webcam(window); 
-//		this.webcam.init();
+		//		this.webcam = webcam(window);
+		//		this.webcam.init();
 	}
 
 	render() {
@@ -40,21 +37,21 @@ constructor(props) {
 			<ExampleApp>
 				<h1 id="title">Take a selfie</h1>
 				<div>
-			        <Webcam
-			          audio={false}
-			          height={350}
-			          ref={this.setRef}
-			          screenshotFormat="image/jpeg"
-			          width={350}
-			        />
-			    <input
+					<Webcam
+						audio={false}
+						height={350}
+						ref={this.setRef}
+						screenshotFormat="image/jpeg"
+						width={350}
+					/>
+					<input
 						type="button"
 						id="takeButton"
 						value="take selfie"
 						onClick={this.capture}
-					/>    
+					/>
 					{this.state.screenshot ? <img src={this.state.screenshot} /> : null}
-			      </div>
+				</div>
 			</ExampleApp>
 		);
 	}
@@ -65,7 +62,3 @@ function goHome() {
 }
 
 export default Home;
-
-
-
-
