480de5282f9a06cd0e206fb7c4803863530b6ad2
Kaho Cheung <kaho@elefanttraks.com>
2018-09-10 10:57:12 +1000

    Cleanup. Ready for merge with develop

--- reducers/index.js
+++ reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 
-import example from './exampleReducer';
 import imageFeed from './imageFeedReducer';
 
 export default combineReducers({
-	example,
 	imageFeed,
 });
